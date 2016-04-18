fdcd1730c192524436d92db25cbaa0a1cd7ad517
genaro colusso <genarocolusso@gmail.com>
2016-04-18 20:53:01 -0300

    started angular, NGrepeat with array lists. for testing UI

--- angularBurger/app/scripts/burgerMenu.js
+++ angularBurger/app/scripts/burgerMenu.js
@@ -1,18 +1,17 @@
 'use strict';
 angular.module('burgerMenu', [])
-  .controller('burgerController', function() {
+  .controller('burgerController', function($scope) {
     var ingredientList = this; 
 
-    ingredientList.ingredient = [
-    	{name:'P'}
+    $scope.ingredient = [
+
+        {name:'Bread', extrainfo:'Delicious bread', price:'2.00', img:'images/paotop.png',type:'food-6.png'},
+    	{name:'Angus Burger', extrainfo:'180g', price:'5.00', img:'images/burgerangus.png',type:'food-2.png'},
+        {name:'Mushrooms', extrainfo:'Fresh Mushrooms', price:'3.00', img:'images/cogumelos.png',type:'food.png'},
+        {name:'Swiss Cheese', extrainfo:'2 Slices', price:'3.50', img:'images/queijosuico.png',type:'cheese.png'}
     ]
 
 
-    todoList.addTodo = function(){ 
-    }; 
-    todoList.remaining = function(){ 
-    }; 
-    todoList.archive = function(){   
-    };
+     
   });
  
