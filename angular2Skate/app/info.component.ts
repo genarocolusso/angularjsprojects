41ac374acf83cc7df4f0c2b316c6338c1fb054c9
genaro colusso <genarocolusso@gmail.com>
2016-09-28 20:13:38 -0300

    newproject

--- angular2Skate/app/info.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'skate-info',
-  template: '<h1>Skate info here</h1>'
-})
-export class InfoComponent { }
--- angular2Skate/app/skate.ts
+++
@@ -1,6 +0,0 @@
-export class Skatepart {
-  constructor(
-    public id: number,
-    public price: number,
-    public name: string) { }
-}
